30c4f9d6b48a42745f6a2f7457db7c78176e70ce
JIWON LEE <iamjwlee93@gmail.com>
2026-01-15 00:52:25 +0900

    pixel2pixel 추가

---
+++ GAN/pixel2pixel.py
@@ -0,0 +1,104 @@
+
+
+
+class pixel2pixel:
+    def __init__(self, gen, dis, device, lr, b1, b2, lambda_pixel):
+        self.device = device
+        self.generator = gen.to(device)
+        self.discriminator = dis.to(device)
+        
+        self.criterion_GAN = torch.nn.MSELoss()
+        self.criterion_pixelwise = torch.nn.L1Loss()
+        self.dataset = _make_dataset()
+        self.lambda_pixel = lambda_pixel
+
+    def _make_dataset(self):
+        data = load_dataset('huggan/facades')
+        dataset = preprocess_img(data)
+        return dataset['train']
+    @staticmethod
+    def preprocess_img(dataset, size=256):
+        transform_ = transforms.Compose(
+            [transforms.Resize((img_h, img_w), Image.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
+        def transform_fn(ex):
+            # Handle grayscale images by converting to RGB if needed
+            # (CelebA is RGB but robust code helps)
+            # However, T.ToImage() handles PIL.
+
+            ex_a = list(map(lambda x : Image.open(io.BytesIO(x['bytes'])).convert('RGB'), ex["imageA"]))
+            ex_b = list(map(lambda x : Image.open(io.BytesIO(x['bytes'])).convert('RGB'), ex["imageA"]))
+            ex["pixel_image_A"] = [transform_(i) for i in ex_a]
+            ex["pixel_image_B"] = [transform_(i) for i in ex_b]
+            return ex
+
+        for split in dataset.keys():
+            dataset[split].set_transform(transform_fn)
+
+        return dataset
+    @staticmethod
+    def collate_pixel_values(batch):
+        # batch: List[dict], 각 dict에 'pixel_values'가 있음
+        return {'a' : torch.stack([b["pixel_image_A"] for b in batch], dim=0), 'b' : torch.stack([b["pixel_image_B"] for b in batch], dim=0)}
+
+    @staticmethod
+    def get_data_loader(
+        dataset: Dataset, batch_size: int, num_workers: int = 3
+    ) -> DataLoader:
+    
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True,
+        collate_fn=collate_pixel_values
+    )
+
+    def train(self, 
+            batch_size,
+            num_epochs,
+            dataloader,
+            lr,
+            b1,
+            b2 ):
+
+        dataloader = get_data_loader(self.dataset, batch_size)
+        optimizer_G = torch.optim.Adam(self.gen.parameters(), lr=lr, betas=(b1, b2))
+        optimizer_D = torch.optim.Adam(self.dis.parameters(), lr=lr, betas=(b1, b2))
+
+        for epoch in range(num_epochs):
+            for i, batch in enumerate(dataloader):
+                real_a = batch['a'].to(self.device)
+                real_b = batch['b'].to(self.device)
+                
+                valid = torch.ones(real_a.size(0), 1).to(self.device)
+                fake = torch.zeros(real_a.size(0), 1).to(self.device)
+                
+                optimizer_G.zero_grad()
+
+                fake_b = self.generator(real_a)
+                pred_fake = self.discriminator(fake_b, real_a)
+                loss_GAN = self.criterion_GAN(pred_fake, valid)
+                loss_pixel = self.criterion_pixelwise(fake_b, real_b)
+
+                loss_G = loss_GAN + self.lambda_pixel * loss_pixel
+                loss_G.backward()
+                optimizer_G.step()
+
+                optimizer_D.zero_grad()
+                
+                pred_real = self.discriminator(real_b, real_a)
+                loss_real = self.criterion_GAN(pred_real, valid)
+                
+                pred_fake = self.discriminator(fake_b.detach(), real_a)
+                loss_fake = self.criterion_GAN(pred_fake, fake)
+                
+                loss_D = (loss_real + loss_fake) / 2
+                loss_D.backward()
+                optimizer_D.step()
+
+                batches_done = epoch * len(data_loader) + i
---
+++ GAN/utils/pix2pixDG.py
@@ -0,0 +1,70 @@
+from utils.custom_layers import UNetDown, UNetUp
+
+
+class GeneratorUNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GeneratorUNet, self).__init__()
+        self.down1 = UNetDown(in_channels, 64, normalize=False)
+        self.down2 = UNetDown(64, 128)
+        self.down3 = UNetDown(128, 256)
+        self.down4 = UNetDown(256, 512, dropout=0.5)
+        self.down5 = UNetDown(512, 512, dropout=0.5)
+        self.down6 = UNetDown(512, 512, dropout=0.5)
+        self.down7 = UNetDown(512, 512, dropout=0.5)
+        self.down8 = UNetDown(512, 512, normalize=False, dropout=0.5)
+        self.up1 = UNetUp(512, 512, dropout=0.5)
+        self.up2 = UNetUp(1024, 512, dropout=0.5)
+        self.up3 = UNetUp(1024, 512, dropout=0.5)
+        self.up4 = UNetUp(1024, 512, dropout=0.5)
+        self.up5 = UNetUp(1024, 256)
+        self.up6 = UNetUp(512, 128)
+        self.up7 = UNetUp(256, 64)
+
+        self.final = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.ZeroPad2d((1,0,1,0)),
+            nn.Conv2d(128, out_channels, 4, padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        d1 = self.down1(x)
+        d2 = self.down2(d1)
+        d3 = self.down3(d2)
+        d4 = self.down4(d3)
+        d5 = self.down5(d4)
+        d6 = self.down6(d5)
+        d7 = self.down7(d6)
+        d8 = self.down8(d7)
+        u1 = self.up1(d8, d7)
+        u2 = self.up2(u1, d6)
+        u3 = self.up3(u2, d5)
+        u4 = self.up4(u3, d4)
+        u5 = self.up5(u4, d3)
+        u6 = self.up6(u5, d2)
+        u7 = self.up7(u6 ,d1)
+        return self.final(u7)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels):
+        super(Discriminator, self).__init__()
+        def discriminator_block(in_channels, out_channels, normalize=True):
+            layers = [nn.Conv2d(in_channels, out_channels, 4, 2, 1)]
+            if normalize:
+                layers.append(nn.BatchNorm2d(out_channels))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *discriminator_block(in_channels * 2, 64, normalize=False),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 256),
+            *discriminator_block(256, 512),
+            nn.ZeroPad2d((1,0,1,0)),
+            nn.Conv2d(512, 1, 4, 1, padding = 1,bias = False)
+        )
+
+    def forward(self, img, img2):
+        img_input = torch.cat((img, img2),1)
+        return self.model(img_input)
